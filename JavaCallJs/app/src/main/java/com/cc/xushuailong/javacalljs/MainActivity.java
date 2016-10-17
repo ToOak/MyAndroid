bb5c23fe24a2a75d51ab1c966c64818a52daa364
shuailong <xushuailong@111.com.cn>
2016-10-17 15:20:57 +0800

    update javaCallJs

--- JavaCallJs/app/src/main/java/com/cc/xushuailong/javacalljs/MainActivity.java
+++ JavaCallJs/app/src/main/java/com/cc/xushuailong/javacalljs/MainActivity.java
@@ -2,7 +2,9 @@ package com.cc.xushuailong.javacalljs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
@@ -12,7 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
+    private WebView webView, webViewGif, webViewImg, webview2;
     private WebSettings webSettings;
     private TextView textView;
     private Button button;
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         webView = (WebView) findViewById(R.id.webview);
+        webViewGif = (WebView) findViewById(R.id.webview_gif);
+        webViewImg = (WebView) findViewById(R.id.webview_img);
+        webview2 = (WebView) findViewById(R.id.webview_2);
         textView = (TextView) findViewById(R.id.arg);
         button = (Button) findViewById(R.id.button);
         buttonWithArg = (Button) findViewById(R.id.buttonWithArg);
@@ -37,26 +42,84 @@ public class MainActivity extends AppCompatActivity {
     private void viewEvents() {
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        setWebview(webSettings);
 //        webView.getSettings().setUseWideViewPort(true);
 //        webView.getSettings().setLoadWithOverviewMode(true);
 //        webView.setWebViewClient(new WebViewClient());
-//        webView.loadUrl("file:///android_asset/index.html");
         webView.loadUrl("file:///android_asset/html/index.html");
-//        webView.addJavascriptInterface(this, "wst");
         webView.addJavascriptInterface(this, "yzjk");
 
+
+        webview2.getSettings().setJavaScriptEnabled(true);
+        webview2.loadUrl("file:///android_asset/index.html");
+        webview2.addJavascriptInterface(this,"wst");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                webView.loadUrl("javascript:javacalljs()");
+                webview2.loadUrl("javascript:javacalljs()");
             }
         });
         buttonWithArg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                webView.loadUrl("javascript:javacalljswitharg(" + "'hello js'" + ")");
+                webview2.loadUrl("javascript:javacalljswitharg(" + "'hello js'" + ")");
             }
         });
+
+
+        setWebview(webViewGif.getSettings());
+        setWebview(webViewImg.getSettings());
+
+        webViewImg.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return (event.getAction() == MotionEvent.ACTION_MOVE);
+            }
+        });
+        webViewGif.loadUrl("file:///android_asset/img/ohno.gif");
+        webViewImg.loadUrl("file:///android_asset/img/bigman.jpg");
+        webViewImg.setScrollContainer(false);
+
+    }
+
+    private void setWebview(WebSettings webSetting) {
+        /**
+         * 用WebView显示图片，可使用这个参数 设置网页布局类型： 1、LayoutAlgorithm.NARROW_COLUMNS ：
+         * 适应内容大小 2、LayoutAlgorithm.SINGLE_COLUMN:适应屏幕，内容将自动缩放
+         */
+//        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSetting.setJavaScriptEnabled(true);
+
+// User settings
+
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSetting.setUseWideViewPort(true);//关键点
+        webSetting.setDisplayZoomControls(false);
+        webSetting.setJavaScriptEnabled(true); // 设置支持javascript脚本
+        webSetting.setAllowFileAccess(true); // 允许访问文件
+        webSetting.setBuiltInZoomControls(true); // 设置显示缩放按钮
+        webSetting.setSupportZoom(false); // 支持缩放
+        webSetting.setLoadWithOverviewMode(true);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int mDensity = metrics.densityDpi;
+        Log.d("maomao", "densityDpi = " + mDensity);
+        if (mDensity == 240) {
+            webSetting.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        } else if (mDensity == 160) {
+            webSetting.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        } else if(mDensity == 120) {
+            webSetting.setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
+        }else if(mDensity == DisplayMetrics.DENSITY_XHIGH){
+            webSetting.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        }else if (mDensity == DisplayMetrics.DENSITY_TV){
+            webSetting.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        }else{
+            webSetting.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        }
     }
 
 
@@ -100,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                textView.setText(textView.getText().toString() + "\njs 调用了 java\n");
+                textView.setText(textView.getText().toString() + "js 调用了 java\n");
             }
         });
     }
@@ -111,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                textView.setText(textView.getText().toString() + "\njs 调用了 java" + str + "\n");
+                textView.setText(textView.getText().toString() + "js 调用了 java" + str + "\n");
             }
         });
     }
