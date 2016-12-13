2c0277b1d877be974924daeb4d44160e5d96aa29
shuailong <xushuailong@111.com.cn>
2016-12-13 15:09:09 +0800

    update

--- MyCrossWalk/app/src/main/java/com/cc/xsl/mycrosswalk/MainActivity.java
+++ MyCrossWalk/app/src/main/java/com/cc/xsl/mycrosswalk/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity {
     }
 
     private void viewEvent() {
+//        webView.loadUrl("file:///android_asset/MyOfficial/index.html");
         webview.load("http://baidu.com",null);
     }
 
